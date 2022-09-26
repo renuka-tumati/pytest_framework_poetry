b35a9e18e99c6c7614019848fb6ce7bc110da493
Renuka Tumati <108145990+renuka-tumati@users.noreply.github.com>
2022-09-26 15:20:43 -0500

    Update json_utils.py

--- helpers/json_utils.py
+++ helpers/json_utils.py
@@ -1,16 +1,24 @@
 import json
   
-# Opening JSON file
-f = open('data.json')
-  
-# returns JSON object as 
-# a dictionary
-data = json.load(f)
-  
-# Iterating through the json
-# list
-for i in data['emp_details']:
-    print(i)
-  
-# Closing file
-f.close()
+def parse_json(path):  
+  # Opening JSON file
+  # './data/test_emp.json'
+  f = open(path)  
+  # returns JSON object as 
+  # a dictionary
+  data = json.load(f)
+  # Closing file
+  f.close()
+  return data
+
+def read_json(path):  
+  # Iterating through the json
+  # list
+  data=parse_json(path)
+  emp_list=list()
+  for i in data['emp_details']:
+      emp=list()
+      emp.append(i["id"])
+      emp.append(i["name"])
+      emp.append(i["department"])
+      emp_list.append(tuple(emp))  
